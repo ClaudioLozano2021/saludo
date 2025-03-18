5ea5915d760fe2c4c835c7b0ac0fe1020396e9a3
Claudio Lozano <claudiolozano@hotmail.com>
2025-03-18 17:31:06 +0100

    Probando action 5

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def saludo(nombre: str) -> str:
     """
     Retorna un saludo personalizado.
     """
-    return f"Hola, {nombre}!"
+    return f"Hola, {nombre}!!"
 
 if __name__ == "__main__":
     mensaje = saludo("Mundo")
